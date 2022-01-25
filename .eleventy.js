6117e7ed348cb0e1e3aa773f40ccac4607c2df74
Chen-Heng CHANG <wandererm@gmail.com>
2022-01-25 09:29:01 +0800

    add syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("images");
 };
